7d7a0c6e06acfca510f960607a4c2da5d8faaa18
Giulia Casal <giulia.ln.casal@gmail.com>
2024-11-07 18:22:46 +0000

    messed it up and fixed again

--- distance_pegs2nucleus.m
+++ distance_pegs2nucleus.m
@@ -1,13 +1,13 @@
 % would you like to load yuour data?
 loadData = 1; % 1 for yes, 0 for no
 
-dataName = 'J7785a_BV1';
+dataName = 'J7571_bv1';
 
 addpath('data')
 if loadData == 1
     clearvars -except dataName;
     PegImg = tiffreadVolume(sprintf('data/%s_10nm_PCpegs.tif',dataName)); % pericyte
-    NucleusImg = tiffreadVolume(sprintf('data/%s_10nm_ECnuc.tif',dataName)); % pericyte
+    NucleusImg = tiffreadVolume(sprintf('data/%s_10nm_PCn.tif',dataName)); % pericyte
 end
 
 mkdir(sprintf('%s',dataName))
@@ -53,7 +53,6 @@ for i = 1:n_peg
 
 end
 
-
 % boundary coord of nucleus
 x_nuc = [];
 y_nuc = [];
@@ -66,9 +65,9 @@ for i = 1 : length(z)
         b_nuc = bwboundaries(nuc);
 
         if isempty(b_nuc)==0
-        x_nuc = [x_nuc x(b_nuc{1}(:,1))];
-        y_nuc = [y_nuc y(b_nuc{1}(:,2))];
-        z_nuc = [z_nuc z(i).*ones(size(y(b_nuc{1}(:,2))))];
+            x_nuc = [x_nuc, x(b_nuc{1}(:,1))];
+            y_nuc = [y_nuc, y(b_nuc{1}(:,2))];
+            z_nuc = [z_nuc, z(i).*ones(size(y(b_nuc{1}(:,2))))];
         end
 
 end
@@ -79,113 +78,60 @@ end
 
 
 d_peg = zeros(n_peg,n_nuc);
-
-% store the min distance co-ordinate of the nucleus
-x_nuc_min = [];
-y_nuc_min = [];
-z_nuc_min = [];
+i_nuc = zeros(n_peg,n_nuc);
 
  % going through all pegs
 for j = 1 : n_peg
 
     % finding distance between peg centre and all nucleus pixels
-    dis = sqrt( ( x_peg(j) - x_nuc ).^2 + ( y_peg(j) - y_nuc ).^2 + ( z_peg(j) - z_nuc) .^2 ) ; 
+    [dis, idx] = min( sqrt( ( x_peg(j) - x_nuc ).^2 + ( y_peg(j) - y_nuc ).^2 + ( z_peg(j) - z_nuc) .^2 ) ); 
 
-    % finding the minimum of those distances
-    [d_peg(j,1), min_idx] = min(dis);
-
-    % storing the x, y, z coordinates of the nucleus with the shortest distance
-    x_nuc_min(j,1) = x_nuc(min_idx);
-    y_nuc_min(j,1) = y_nuc(min_idx);
-    z_nuc_min(j,1) = z_nuc(min_idx);
+    d_peg(j) = dis;
+    i_nuc(j) = idx;
 
 end
 
 T = array2table([x_peg, y_peg, z_peg, d_peg]);
 T.Properties.VariableNames = {'x','y','z','d'};
 writetable(T,sprintf('%s/pegPositions.csv',dataName))
-%csvwrite(sprintf('%s/pegPositions.csv'),[x_peg, y_peg, z_peg, d_peg])
+csvwrite(sprintf('%s/pegPositions.csv'),[x_peg, y_peg, z_peg, d_peg])
 % n_peg, x_peg, y_peg, z_peg, d_peg,
 
-
-% figure(9); cla; hold on;
-% plot3(x_peg,y_peg,z_peg,'ko')
-
-
 figure(8); cla; hold on; axis equal; set(gca,'box','on')
+
+% peg centroids
+% if data does not line up 1. change values x, y here 
 plot3(y_peg,x_peg,z_peg,'ko')
 
+%drawing peg and nucleus boundary
+% for EC nucleus blue [179,205,227]./255 , for PC nucleus orange [250, 129, 47]./255
 for i = 1 : length(z)
 
     peg(:,:) =  PegImg(:,:,i);
 
-     nuc(:,:) =  NucleusImg(:,:,i);
+    nuc(:,:) =  NucleusImg(:,:,i);
 
     b_peg = bwboundaries(peg);
     b_nuc = bwboundaries(nuc);
 
     for j = 1 : length(b_peg)
-        plot3( y(b_peg{j}(:,1)), x(b_peg{j}(:,2)),z(i).*ones(size(b_peg{j}(:,2))), 'color', [228,26,28]./255)
+        plot3( x(b_peg{j}(:,1)), y(b_peg{j}(:,2)),z(i).*ones(size(b_peg{j}(:,2))), 'color', [228,26,28]./255)
     end
 
+% if data does not line up 2. change values x, y here 
     for j = 1 : length(b_nuc)
-        plot3( y(b_nuc{j}(:,1)), x(b_nuc{j}(:,2)),z(i).*ones(size(b_nuc{j}(:,2))), 'color', [179,205,227]./255  )
+        plot3( x(b_nuc{j}(:,1)), y(b_nuc{j}(:,2)),z(i).*ones(size(b_nuc{j}(:,2))), 'color', [179,205,227]./255  )
     end
 
     pause(0); drawnow
 end
-% Script to plot histogram with smoothed distribution line in micrometers
-% Make sure `d_peg` data is loaded in your MATLAB workspace before running this script
 
-% Convert d_peg from nanometers to micrometers
-d_peg_micrometers = d_peg / 1000;
+% draws shortest path between each peg and nucleus
+% if data does not line up 3. change values x, y here 
+for j = 1 : n_peg
+    plot3([x_nuc(i_nuc(j)) y_peg(j)],[y_nuc(i_nuc(j)) x_peg(j)],[z_nuc(i_nuc(j)) z_peg(j)],'--k')
 
-% Plot the histogram with 20 bins, normalized as probability density
-figure;
-histogram(d_peg_micrometers, 'NumBins', 20, 'Normalization', 'pdf');
-hold on;
-
-% Calculate the histogram counts and bin edges
-[counts, binEdges] = histcounts(d_peg_micrometers, 'NumBins', 20, 'Normalization', 'pdf');
-binCenters = binEdges(1:end-1) + diff(binEdges)/2;
-
-% Interpolate to smooth the line with 200 points
-xq = linspace(min(binCenters), max(binCenters), 200); % More points for a smoother curve
-smoothedCounts = interp1(binCenters, counts, xq, 'spline'); % Spline interpolation for smoothness
-
-% Set any negative values in the smoothed line to zero
-smoothedCounts(smoothedCounts < 0) = 0;
-
-% Plot the smoothed line
-plot(xq, smoothedCounts, 'LineWidth', 2);
-
-% Add labels and title
-xlabel('Distance (\mum)'); % Update units to micrometers
-ylabel('Probability Density');
-title('Histogram of Minimum Distances with Smoothed Distribution Curve in Micrometers');
-grid on;
-
-% Release the hold on the current figure
-hold off;
-
-% Release the hold on the current figure
-hold off;
-
-
-% for j = 1:n_peg
-%     % Get the starting point (peg centroid)
-%     startY = x_peg(j);
-%     startX = y_peg(j);
-%     startZ = z_peg(j);
-%   
-%     endY = x_nuc_min(j);
-%     endX = y_nuc_min(j);
-%     endZ = z_nuc_min(j);
-%   
-%     plot3([startX, endX], [startY, endY], [startZ, endZ], 'k--');
-% 
-%     pause(0); drawnow
-% end
+end
 
 view(45,15)
 % exportgraphics(gcf,sprintf('%s/3Dplot.png',dataName),'resolution',1000)
